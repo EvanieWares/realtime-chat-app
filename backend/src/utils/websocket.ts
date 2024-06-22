445992b003d25f2d9dcf89c1416fdf1c95ad98bb
EvanieWares <evaniewares@gmail.com>
2024-06-22 11:01:10 +0200

    Add JSDoc to websockets.ts

--- backend/src/utils/websocket.ts
+++ backend/src/utils/websocket.ts
@@ -2,10 +2,12 @@ import { Server } from "socket.io";
 import Message from "../models/messageModel";
 
 /**
- * Sets up a WebSocket server and listens for connections. When a connection is established, it logs a message to the console.
- * When a "chat message" event is received, it creates a new Message object with the user and message data, saves it to the database,
- * and emits a "chat message" event to all connected clients with the new message.
- * When a "disconnect" event is received, it logs a message to the console and disconnects the client.
+ * Sets up a WebSocket server and listens for connections. When a connection
+ * is established, it logs a message to the console. When a "chat message"
+ * event is received, it creates a new Message object with the user and message
+ * data, saves it to the database, and emits a "chat message" event to all
+ * connected clients with the new message. When a "disconnect" event is received,
+ * it logs a message to the console and disconnects the client.
  *
  * @param {Server} io - The Socket.IO server instance.
  * @return {void} This function does not return anything.
