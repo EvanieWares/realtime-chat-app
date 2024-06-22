89177ca0498c24844220fc9809dbf5287afcbc38
EvanieWares <evaniewares@gmail.com>
2024-06-22 10:49:50 +0200

    Add JSDoc to authController.ts

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,13 +3,22 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 
-const register = async (req: Request, res: Response) => {
+/**
+ * Asynchronously registers a new user.
+ *
+ * @param {Request} req - The request object containing user data.
+ * @param {Response} res - The response object to send back the result.
+ * @return {Promise<void>} Resolves with a success message or rejects
+ *                        with an error message.
+ */
+const register = async (req: Request, res: Response): Promise<void> => {
   const { username, password } = req.body;
 
   try {
     let user = await User.findOne({ username });
     if (user) {
-      return res.status(400).json({ message: "User with that username already exists." });
+      res.status(400).json({ message: "User with that username already exists." });
+      return;
     }
 
     user = new User({ username, password });
@@ -35,14 +44,24 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
-const login = async (req: Request, res: Response) => {
+/**
+ * Authenticates a user login by checking the username and password against
+ * the database.
+ *
+ * @param {Request} req - The request object containing username and password.
+ * @param {Response} res - The response object to send back the result.
+ * @return {Promise<void>} Resolves with a JWT token if login is successful,
+ *                        or rejects with an error message if login fails.
+ */
+const login = async (req: Request, res: Response): Promise<void> => {
   const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ username });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: "Invalid credentials." });
+      res.status(401).json({ message: "Invalid credentials." });
+      return;
     }
 
     const payload = {
