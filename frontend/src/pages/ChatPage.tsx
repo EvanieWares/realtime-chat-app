6f961b58c896248a7f971443904199bfcdc4de34
EvanieWares <evaniewares@gmail.com>
2024-06-23 08:24:30 +0200

    Create a chat page

---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatBox from '../components/ChatBox';
+
+const ChatPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Chat Room</h1>
+      <ChatBox />
+    </div>
+  );
+};
+
+export default ChatPage;
