1abfd87e72c9b4c0a5028aa51eaa1c9433580e84
EvanieWares <evaniewares@gmail.com>
2024-06-23 08:24:55 +0200

    Create the login page

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from '../components/Login';
+import Form from '../components/Form';
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default LoginPage;
