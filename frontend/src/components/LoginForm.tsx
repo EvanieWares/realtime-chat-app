a53cac5063147ba8edff54cb0f7e673d96b66c6d
EvanieWares <evaniewares@gmail.com>
2024-06-24 10:21:10 +0200

    Add enter keypress action and handle errors gracefully

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,54 +1,85 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { FaLock, FaUser } from "react-icons/fa";
 import { UserContext } from "../context/UserContext";
 import { login, register } from "../services/api";
 import "../styles/form.css";
+import { AxiosError } from "axios";
 
 const LoginForm: React.FC = () => {
   const { setUser } = useContext(UserContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isRegister, setIsRegister] = useState(false);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
 
   const handleLogin = async () => {
+    setError("");
+    if (username === "" || password === "") {
+      setError("Please enter a username and password");
+      return;
+    }
     try {
       const data = await login(username, password);
       localStorage.setItem('token', data.token);
       setUser(username);
-    } catch (error) {
-      console.error("Login failed:", error);
-      alert("Login failed. Please try again.");
+    } catch (error: AxiosError | any) {
+      setError(error.response.data.message);
     }
   };
 
   const handleRegister = async () => {
+    setError("");
+    if (username === "" || password === "") {
+      setError("Please enter a username and password");
+      return;
+    }
     try {
       const data = await register(username, password);
       localStorage.setItem('token', data.token);
       setUser(username);
-    } catch (error) {
-      console.error("Registration failed:", error);
+    } catch (error: AxiosError | any) {
+      setError(error.response.data.message);
+    }
+  };
+
+  const handleOnLoginRegisterClick = () => {
+    setError("");
+    setIsRegister(!isRegister);
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>, done: boolean) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      if (done) {
+        isRegister ? handleRegister() : handleLogin();
+      } else {
+        passwordInputRef.current?.focus();
+      }
     }
   };
 
   return (
     <div className="login-wrapper">
       <h1>{isRegister ? 'Register' : 'Login'}</h1>
-      <div className="input-box">
+      <div className={error === "" ? "input-box" : "input-box error"}>
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={(event) => { handleKeyDown(event, false) }}
           placeholder="Username"
           required
         />
         <FaUser className="icon" />
       </div>
-      <div className="input-box">
+      <div className={error === "" ? "input-box" : "input-box error"}>
         <input
           type="password"
           value={password}
+          ref={passwordInputRef}
           onChange={(e) => setPassword(e.target.value)}
+          onKeyDown={(event) => { handleKeyDown(event, true) }}
           placeholder="Password"
           required
         />
@@ -59,8 +90,11 @@ const LoginForm: React.FC = () => {
       >
         {isRegister ? 'Register' : 'Login'}
       </button>
+      <div className="error-message"> {error} </div>
       <div className="register-link">
-        <p>Don't have an account? <a href="#" onClick={() => setIsRegister(!isRegister)}>Register</a></p>
+        <p>{isRegister ? "Already have an account? " : "Don't have an account? "}
+          <a href="#" onClick={() => handleOnLoginRegisterClick()}>{isRegister ? 'Login' : 'Register'}</a>
+        </p>
       </div>
     </div>
   )
